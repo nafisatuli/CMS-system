0df71b3e83f656b3b93ee4b8fbd93eb7efeb50d8
Nafisa Hasan Tuli <nafisahasan98@gmail.com>
2020-10-10 01:29:06 +0600

    add delete feature in All post

--- routes/admin/posts.js
+++ routes/admin/posts.js
@@ -99,4 +99,14 @@ router.put('/edit/:id', (req, res) => {
     //res.send('It works');
 });
 
+router.delete('/:id', (req, res) => {
+    Post.remove({
+        _id: req.params.id
+    }).then(result => {
+        res.redirect('/admin/posts');
+    }).catch(error => {
+        console.log(error);
+    });
+});
+
 module.exports = router;
