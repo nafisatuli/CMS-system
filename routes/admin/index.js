8841c092708aa50e79a982626250be04a5974476
Nafisa Hasan Tuli <nafisahasan98@gmail.com>
2020-10-10 12:32:36 +0600

    create dummy data

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const {
+    route
+} = require('../home');
 const router = express.Router();
+const faker = require('faker');
+const Post = require('../../models/Post');
 
 
 //override default layout
@@ -16,5 +21,24 @@ router.get('/', (req, res) => {
 
 //here we dont have to get /admin bcz in the middleware we already told this
 //if here another routes client so it will be admin/client already
+
+//for dummy data creation
+router.post('/generate-fake-posts', (req, res) => {
+
+    for (let i = 0; i < req.body.amount; i++) {
+        let post = new Post();
+        post.title = faker.name.title();
+        post.status = 'public';
+        post.allowComments = faker.random.boolean();
+        post.body = faker.lorem.sentence();
+        post.save(function (err) {
+            if (err) throw err;
+        });
+
+    }
+
+    res.redirect('/admin/posts');
+});
+
 
 module.exports = router;
