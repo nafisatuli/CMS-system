830b399b692a4c4f508889fccf562f2c8ab3b163
Nafisa Hasan Tuli <nafisahasan98@gmail.com>
2021-01-04 19:47:24 +0600

    Remove dist folders

--- config/dist/database.dev.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-module.exports = {
-  mongoDbUrl: "mongodb+srv://".concat(process.env.DATABASE_USERNAME, ":").concat(process.env.DATABASE_PASSWORD, "@cluster0.e6jji.mongodb.net/maven-cms?retryWrites=true&w=majority")
-};
