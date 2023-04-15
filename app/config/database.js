b13fed639b26d45a16b68a5ae1fb561743e14d51
Ans <anssaeed.pk@gmail.com>
2023-04-15 12:18:27 +0500

    Restructured App folders

---
+++ app/config/database.js
@@ -0,0 +1,7 @@
+module.exports = {
+    // Use for local depolyment with node start
+    // mongoDbUrl: `mongodb://admin:password@localhost:27017`
+
+    // Use when running as a docker container
+    mongoDbUrl: 'mongodb://admin:password@mongodb:27017'
+};
--- config/database.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    mongoDbUrl: `mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@cluster0.e6jji.mongodb.net/maven-cms?retryWrites=true&w=majority`
-};
