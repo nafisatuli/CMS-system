0d47af0d4a3aebf44d8192c2141975bd133a8b36
Ans <anssaeed.pk@gmail.com>
2023-04-19 14:13:30 +0500

    Updated script.groovy file

--- script.groovy
+++ script.groovy
@@ -1,9 +1,14 @@
 def buildAPP(){
     echo "building the application..."
+    sh 'npm run build'
 }
 
-def testAPP(){
-    echo "testing the application..."
+def buildImage(){
+    echo "building the docker image of application..."
+    sh 'docker build -t anssaeed/my-repo:cms1.0 .'
+    sh 'echo $PASS | docker login -u $USER --password-stdin'
+    sh 'docker push anssaeed/my-repo:cms1.0'
+
 }
 
 def deployAPP(){
