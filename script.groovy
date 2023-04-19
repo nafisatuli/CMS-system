dab7e437b51868817f06180eb71bddb1bbfba6d2
Ans <anssaeed.pk@gmail.com>
2023-04-19 14:17:05 +0500

    commented script.groovy

--- script.groovy
+++ script.groovy
@@ -11,8 +11,8 @@ def buildImage(){
 
 }
 
-def deployAPP(){
-    echo "deploying the application..."
-    echo "deploying version ${params.VERSION}"
-}
+// def deployAPP(){
+//     echo "deploying the application..."
+//     echo "deploying version ${params.VERSION}"
+// }
 return this
